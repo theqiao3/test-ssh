63a721960d7445a97d8d63a91337a96442d8faab
qqzz <2157283079@qq.com>
2025-08-09 16:48:56 +0800

    branch2

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 np.random.seed(42)
 np.abs(np.random.randn(1000))
-if __name__ == "__main__":
-    print("Random numbers generated and absolute values taken.")
-    print("First 10 values:", np.abs(np.random.randn(10)))
-    sys.exit(0) 
+plt.hist(np.abs(np.random.randn(1000)), bins=30, density=True, alpha=0.6, color='g')
+plt.title('Histogram of Absolute Values of Random Numbers')
+plt.xlabel('Value')
 
