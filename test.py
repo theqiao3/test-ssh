6a5dbfdc16dc4cc09b31cda54097bd808d20bb58
qqzz <2157283079@qq.com>
2025-08-09 17:08:57 +0800

    branch2-1

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+# Check if the script is being run directly
 np.random.seed(42)
 np.abs(np.random.randn(1000))
 plt.hist(np.abs(np.random.randn(1000)), bins=30, density=True, alpha=0.6, color='g')
