3afff4f866755683accc2b6f89ce00ce8a1fa5fb
qqzz <2157283079@qq.com>
2025-08-09 17:32:44 +0800

    srring-test1

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hello git")
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# 示例测试
+a = np.array([1, 2, 3, 4, 5])
+b = np.array([5, 4, 3, 2, 1])
+c = a + b
+print("a + b =", c)
