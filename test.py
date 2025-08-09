99d8080d0237ec13119c67e0d755995fc687df0e
qqzz <2157283079@qq.com>
2025-08-09 17:28:25 +0800

    branch1-3

--- test.py
+++ test.py
@@ -2,4 +2,9 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+print("hello git")
+# 示例测试
+a = np.append(np.array([1, 2, 3]), np.array([4, 5, 6]))
+np.amax(a)
+plt.plot(a)
+plt.show()
